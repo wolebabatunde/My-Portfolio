6f62492dadc79a2d45de72789abc060a19022bcb
github_wolebabatunde <kolawole_yekini@yahoo.com>
2020-05-23 22:54:47 +0100

    landing

--- my-portfolio/src/components/LandingPage.js
+++ my-portfolio/src/components/LandingPage.js
@@ -1,13 +1,33 @@
 import React, {Component} from 'react';
+import {Grid, Cell} from 'react-mdl';
 
 class LandingPage extends Component {
     render(){
         return(
-            <div>
-                <h1>Landing Page</h1>
+            <div style={{width:'100', margin:'auto'}}>
+                <Grid className="landingpage-grid">
+                    <Cell col={12}>
+                        <img
+                        src="https://mir-s3-cdn-cf.behance.net/user/276/3d59ed371363557.5d152e268c7ca.jpg"
+                        alt="avater"
+                        className="avater-img"
+                        />
+
+                        <div className="banner-text">
+                            <h2>User Interface & User Experience Designer</h2>
+
+                            <hr/>
+
+                            <p>HTML / HTML5 | CSS / CSS3 | BOOTSTRAP | REACT | ADOBE XD | FIGMA</p>
+                            
+                        </div>
+                        
+                    </Cell>
+                </Grid>
+                
             </div>
         )
-    }
+}
 }
 
 export default LandingPage;
